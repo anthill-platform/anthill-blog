71cbd554379ff4e165e0bff54d93e008edb809bd
Alex Siryi <desertkun@gmail.com>
2020-05-24 22:28:50 +0300

    ditched pypigit

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 DEPENDENCIES = [
-    "anthill-common"
+    "anthill-common>=0.2.4"
 ]
 
 setup(
@@ -10,8 +10,7 @@ setup(
     package_data={
       "anthill.event": ["anthill/blog/sql", "anthill/blog/static"]
     },
-    setup_requires=["pypigit-version"],
-    git_version="0.1.0",
+    version='0.2',
     description='A service to deliver news and patch notes feed to the users inside the game',
     author='desertkun',
     license='MIT',
@@ -19,7 +18,7 @@ setup(
     url='https://github.com/anthill-platform/anthill-blog',
     namespace_packages=["anthill"],
     include_package_data=True,
-    packages=find_packages(),
+    packages=find_namespace_packages(include=["anthill.*"]),
     zip_safe=False,
     install_requires=DEPENDENCIES
 )
